53dbf9bc2afa26996f17d1118cba86ae0592c9a6
JohannesMerz <johannes_merz@gmx.de>
2019-10-15 18:59:40 +0200

    base setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  margin: 2rem;
+  padding: 2rem;
+  border: 2px solid grey;
+`
+
+export default function(props) {
+  return <Container>hello world</Container>
+}
