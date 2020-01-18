4f5a9621a4716b9bf79a7f5792325ff8656e9b1d
JohannesMerz <johannes_merz@gmx.de>
2020-01-18 21:45:37 +0100

    fix story

--- src/components/Box.bojagi.js
+++ src/components/Box.bojagi.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Box } from './Box';
-
-export default { title: 'Box' };
+import Box from './Box';
 
 export const yellow = () => <Box color="yellow">I am a yellow Box!</Box>;
 
