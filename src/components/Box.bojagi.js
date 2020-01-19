8b51439815ffe45d602c0d2f9ee99a21e3e40a82
Johannes Merz <JohannesMerz@users.noreply.github.com>
2020-01-19 15:34:52 +0100

    Update Box.bojagi.js

--- src/components/Box.bojagi.js
+++ src/components/Box.bojagi.js
@@ -6,3 +6,5 @@ export const yellow = () => <Box color="yellow">I am a yellow Box!</Box>;
 export const green = () => <Box color="green">I am so green!</Box>
 
 export const red = () => <Box color="red">I am so green! I mean red...</Box>
+
+export const blue = () => <Box color="blue">Blue.</Box>
