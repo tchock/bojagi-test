5625ff379f1360076f12c8cf674ecaf1406354c4
JohannesMerz <johannes_merz@gmx.de>
2020-02-09 12:18:51 +0100

    blackbox

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import Box from './components/Box'
 import Button from './components/Button'
 import BoxWithButtons from './components/BoxWithButtons'
+import BlackBox from './components/BlackBox'
 import { yellow } from './components/Box.bojagi'
 
 const Container = styled.div`
@@ -16,6 +17,7 @@ export default function(props) {
     <Container>
       <Box color="green">green><Button color="yellow">click</Button></Box>
       <Box color="yellow">yellow</Box>
+      <BlackBox>Hi!</BlackBox>
       <BoxWithButtons color="white" buttons={[{text: 'stop', color: 'red'},{text: 'ready', color: 'yellow'}, {text: 'go', color: 'green'}]}/>
     </Container>
   )
---
+++ src/components/BlackBox.js
@@ -0,0 +1,10 @@
+// @component
+import React from 'react'
+import styled from 'styled-components'
+
+import Box from './Box'
+
+
+export default function BlackBox(props) {
+  return <Box {...props} color={"black"} />
+}
