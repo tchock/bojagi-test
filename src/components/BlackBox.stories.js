27ecde2b77a5823647d06d95ba0e77dbc8d45d7d
JohannesMerz <johannes_merz@gmx.de>
2020-11-18 20:34:10 +0100

    commit 2

--- src/components/BlackBox.stories.js
+++ src/components/BlackBox.stories.js
@@ -5,6 +5,6 @@ export default { title: 'BlackBox' };
 
 export const hello = () => <BlackBox color="yellow">Hellooo!</BlackBox>;
 
-export const bye = () => <BlackBox color="green">Bye!</BlackBox>
+export const bye = () => <BlackBox color="green">Bye :(!</BlackBox>
 
 export const click = () => <BlackBox color="red">Click!</BlackBox>
