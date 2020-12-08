ad5ee30bc6f3301aac6494382f5d2e4d241511d5
JohannesMerz <johannes_merz@gmx.de>
2020-12-08 22:47:36 +0100

    moar moar changes

--- src/components/Box.bojagi.js
+++ src/components/Box.bojagi.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Box from './Box';
 
-export const yellow = () => <Box color="yellow">I am a yellow Box!!</Box>;
+export const yellow = () => <Box color="yellow">I am a yellow Box!!!</Box>;
 
 export const green = () => <Box color="green">I am so green!</Box>
 
