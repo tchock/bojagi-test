38adec14f7a2fbbed436b1a8ce80ed042d4d55ed
JohannesMerz <johannes_merz@gmx.de>
2021-02-11 18:30:52 +0100

    up bojagi cli

--- .bojagirc.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  storyPath: './src/**/*.stories.js'
-}
--- .storybook/main.js
+++ .storybook/main.js
@@ -2,8 +2,6 @@ module.exports = {
   stories: ['../src/**/*.stories.js'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
   webpackFinal: async config => {
-    // do mutation to the config
-
     return config;
   },
 };
